717767c0a1ebd468f0885ffc49aa1aef4eadbb06
Matthew Stickney <fiendishlinux@gmail.com>
2013-06-11 15:24:19 -0400

    Add WITH-CONSOLE macro

--- trivial-debug-console.lisp
+++ trivial-debug-console.lisp
@@ -2,19 +2,25 @@
 
 (in-package #:trivial-debug-console)
 
+;; TODO: don't clobber the previous window state
+;; TODO: Handle the case of a null console better (e.g. GUI apps)
+(defmacro with-console ((window) &body body)
+  (let ((window-var (gensym)))
+    `(let ((,window-var ,window))
+       (when (not (cffi:null-pointer-p ,window-var))
+         (show-window ,window-var :restore))
+       (unwind-protect (progn ,@body)
+         (when (not (cffi:null-pointer-p ,window-var))
+           (show-window ,window-var :hide))))))
+
 (defun debug-handler (&optional (parent-hook *debugger-hook*))
   "Return a function to be used as *DEBUGGER-HOOK* that shows the console window, runs any existing hooks, and hides the window when debugging is complete."
   (lambda (condition hook)
     (let ((w (console-window)))
-      ;; Show the console
-      (when (not (cffi:null-pointer-p w))
-        (show-window w :restore))
-      ;; Run the existing hook/enter the standard debugger
-      (unwind-protect (progn (when parent-hook
-                               (funcall parent-hook condition parent-hook))
-                             (invoke-debugger condition))
-        (when (not (cffi:null-pointer-p w))
-          (show-window w :hide))))))
+      (with-console (w)
+        (when parent-hook
+          (funcall parent-hook condition parent-hook))
+        (invoke-debugger condition)))))
 
 (defun debuggify (func &rest args)
   (let* ((*debugger-hook* (debug-handler)))
